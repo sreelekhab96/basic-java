c6ec98017b76467c8581939c9079b457b735c297
sreelekhab96 <sreelekhab96@gmail.com>
2020-05-17 21:24:49 +0530

    first commit by sree

--- Demo1.java
+++ Demo1.java
@@ -1,5 +1,7 @@
 class Demo{
-	public static void main(String jenny[]){
+	public static void main(String jenny[])
+	//this is comment
+	{
 		System.out.println("Welcome to Java Programming Language");
 	}
 }
